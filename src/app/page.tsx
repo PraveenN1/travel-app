eaaa0f1f9caea3d15a9ff8bc45dd996edf978394
PraveenN1 <praveenpu46@gmail.com>
2024-06-24 01:12:07 +0530

    Components Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
+
+    </>
   );
 }
