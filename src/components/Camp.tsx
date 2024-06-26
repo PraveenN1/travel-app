23cb2c5b0938b366d9faca7cdfad70cadd508761
PraveenN1 <praveenpu46@gmail.com>
2024-06-26 23:14:13 +0530

    Updated Camp section

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -1,9 +1,108 @@
-import React from 'react'
+"use client"
+import React, { useRef, useState, MouseEvent } from 'react';
+import styles from '../app/horizontalscroll.module.css';
+import Image from 'next/image';
 
-const Camp = () => {
-  return (
-    <div>Camp</div>
-  )
-}
+const HorizontalScroll: React.FC = () => {
+    const scrollWrapperRef = useRef<HTMLDivElement>(null);
+    const [isDown, setIsDown] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
 
-export default Camp
+    const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+        if (scrollWrapperRef.current) {
+            setIsDown(true);
+            setStartX(e.pageX - scrollWrapperRef.current.offsetLeft);
+            setScrollLeft(scrollWrapperRef.current.scrollLeft);
+            scrollWrapperRef.current.style.cursor = 'grabbing';
+        }
+    };
+
+    const handleMouseLeave = () => {
+        setIsDown(false);
+        if (scrollWrapperRef.current) {
+          scrollWrapperRef.current.style.cursor = 'grab';
+      }
+    };
+
+    const handleMouseUp = () => {
+        setIsDown(false);
+        if (scrollWrapperRef.current) {
+          scrollWrapperRef.current.style.cursor = 'grab';
+      }
+    };
+
+    const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+        if (!isDown) return;
+        e.preventDefault();
+        if (scrollWrapperRef.current) {
+            const x = e.pageX - scrollWrapperRef.current.offsetLeft;
+            const walk = (x - startX) * 3; //scroll-fast
+            scrollWrapperRef.current.scrollLeft = scrollLeft - walk;
+        }
+    };
+
+    const scrollLeftClick = () => {
+        if (scrollWrapperRef.current) {
+            scrollWrapperRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+        }
+    };
+
+    const scrollRightClick = () => {
+        if (scrollWrapperRef.current) {
+            scrollWrapperRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <div className={styles.scrollContainer}>
+            <button className={`${styles.scrollButton} ${styles.left}`} onClick={scrollLeftClick}>
+                &#9664;
+            </button>
+            <div
+                className={styles.scrollWrapper}
+                ref={scrollWrapperRef}
+                onMouseDown={handleMouseDown}
+                onMouseLeave={handleMouseLeave}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+            >
+                <div className={styles.card}>
+                  <div className={styles.overlay}>
+                        <div className={styles.text}>Text on Image 1</div>
+                    </div>
+                  <Image
+                    src="/img-1.png"
+                    alt="cardimg"
+                    width={800}
+                    height={800}
+                    className='rounded-3xl'
+                  />
+                </div>
+                <div className={styles.card}>
+                  <Image
+                    src="/img-2.png"
+                    alt="cardimg"
+                    width={800}
+                    height={800}
+                    className='rounded-3xl'
+                  />
+                </div>
+                <div className={styles.card}>
+                  <Image
+                    src="/img-3.png"
+                    alt="cardimg"
+                    width={800}
+                    height={800}
+                    className='rounded-3xl'
+                  />
+                </div>
+            </div>
+            <button className={`${styles.scrollButton} ${styles.right}`} onClick={scrollRightClick}>
+                &#9654;
+            </button>
+        </div>
+    );
+};
+
+export default HorizontalScroll;
