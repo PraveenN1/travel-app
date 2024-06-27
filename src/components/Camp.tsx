c4bced8f47c4223ddd2308865a98338a9228028a
PraveenN1 <praveenpu46@gmail.com>
2024-06-27 15:37:28 +0530

    Added Guide Section

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -1,108 +1,89 @@
-"use client"
-import React, { useRef, useState, MouseEvent } from 'react';
-import styles from '../app/horizontalscroll.module.css';
-import Image from 'next/image';
+import { PEOPLE_URL } from "../../constants";
+import Image from "next/image";
 
-const HorizontalScroll: React.FC = () => {
-    const scrollWrapperRef = useRef<HTMLDivElement>(null);
-    const [isDown, setIsDown] = useState(false);
-    const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
 
-    const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-        if (scrollWrapperRef.current) {
-            setIsDown(true);
-            setStartX(e.pageX - scrollWrapperRef.current.offsetLeft);
-            setScrollLeft(scrollWrapperRef.current.scrollLeft);
-            scrollWrapperRef.current.style.cursor = 'grabbing';
-        }
-    };
-
-    const handleMouseLeave = () => {
-        setIsDown(false);
-        if (scrollWrapperRef.current) {
-          scrollWrapperRef.current.style.cursor = 'grab';
-      }
-    };
-
-    const handleMouseUp = () => {
-        setIsDown(false);
-        if (scrollWrapperRef.current) {
-          scrollWrapperRef.current.style.cursor = 'grab';
-      }
-    };
-
-    const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-        if (!isDown) return;
-        e.preventDefault();
-        if (scrollWrapperRef.current) {
-            const x = e.pageX - scrollWrapperRef.current.offsetLeft;
-            const walk = (x - startX) * 3; //scroll-fast
-            scrollWrapperRef.current.scrollLeft = scrollLeft - walk;
-        }
-    };
-
-    const scrollLeftClick = () => {
-        if (scrollWrapperRef.current) {
-            scrollWrapperRef.current.scrollBy({ left: -200, behavior: 'smooth' });
-        }
-    };
-
-    const scrollRightClick = () => {
-        if (scrollWrapperRef.current) {
-            scrollWrapperRef.current.scrollBy({ left: 200, behavior: 'smooth' });
-        }
-    };
-
-    return (
-        <div className={styles.scrollContainer}>
-            <button className={`${styles.scrollButton} ${styles.left}`} onClick={scrollLeftClick}>
-                &#9664;
-            </button>
-            <div
-                className={styles.scrollWrapper}
-                ref={scrollWrapperRef}
-                onMouseDown={handleMouseDown}
-                onMouseLeave={handleMouseLeave}
-                onMouseUp={handleMouseUp}
-                onMouseMove={handleMouseMove}
-            >
-                <div className={styles.card}>
-                  <div className={styles.overlay}>
-                        <div className={styles.text}>Text on Image 1</div>
-                    </div>
-                  <Image
-                    src="/img-1.png"
-                    alt="cardimg"
-                    width={800}
-                    height={800}
-                    className='rounded-3xl'
-                  />
-                </div>
-                <div className={styles.card}>
-                  <Image
-                    src="/img-2.png"
-                    alt="cardimg"
-                    width={800}
-                    height={800}
-                    className='rounded-3xl'
-                  />
-                </div>
-                <div className={styles.card}>
-                  <Image
-                    src="/img-3.png"
-                    alt="cardimg"
-                    width={800}
-                    height={800}
-                    className='rounded-3xl'
-                  />
-                </div>
-            </div>
-            <button className={`${styles.scrollButton} ${styles.right}`} onClick={scrollRightClick}>
-                &#9654;
-            </button>
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+     <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+      <div className="flexCenter gap-4">
+        <div className="rounded-full bg-green-50 p-4">
+          <Image
+            src="/folded-map.svg"
+            alt="map"
+            width={28}
+            height={28}
+          />
         </div>
-    );
-};
+        <div className="flex flex-col gap-1">
+          <h4 className="bold-18 text-white">{title}</h4>
+          <p className="regular-14 text-white">{subtitle}</p>
+        </div>
+      </div>
 
-export default HorizontalScroll;
+      <div className="flexCenter gap-6">
+        <span className="flex -space-x-4 overflow-hidden">
+          {PEOPLE_URL.map((url) => (
+            <Image 
+              className="inline-block h-10 w-10 rounded-full"
+              src={url}
+              key={url}
+              alt="person"
+              width={52}
+              height={52}
+            />
+          ))}
+        </span>
+        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+const Camp = () => {
+  return (
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite 
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite 
+          backgroundImage="bg-bg-img-2"
+          title="Mountain View Camp"
+          subtitle="Somewhere in the Wilderness"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          </p>
+          <Image 
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Camp
