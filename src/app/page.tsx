5894300329a57ae1a0e25f78482e6953c21e37e5
PraveenN1 <praveenpu46@gmail.com>
2024-06-27 17:45:58 +0530

    Added GetApp Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Guide/>
       <Features/>
       <GetApp/>
-
+      
     </>
   );
 }
--- src/components/GetApp.tsx
+++ src/components/GetApp.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
+    <section className='flexCenter w-full pb-[100px]'>
+      <div className='get-app'>
+        {/* Left */}
+        <div className='flex flex-1 flex-col z-20 items-start justify-center '>
+            <h2 className='bold-52 text-balance'>Get for <span className='text-green-500'>free</span> now !</h2>
+            <p className='regular-20 py-10'>Available on iOS and Android</p>
+
+          <div className='flex flex-col w-full lg:flex-row gap-2 whitespace-nowrap' >
+            <Button
+              type='button'
+              title='App Store'
+              icon='apple.svg'
+              variant='btn_white'
+            />
+            <Button
+              type='button'
+              title='Play Store'
+              icon='android.svg'
+              variant='btn_dark_green'
+            />
+          </div>
+        </div>
+        {/* Right */}
+        <div className='flex flex-1 items-center justify-end'>
+          <Image
+            src="/phones.png"
+            alt="phones"
+            width={480}
+            height={640}
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
