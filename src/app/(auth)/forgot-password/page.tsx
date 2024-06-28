2210ae0c4f61814569afe7b868391307bb55f126
PraveenN1 <praveenpu46@gmail.com>
2024-06-28 19:10:02 +0530

    Added auth

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+import { defaultConfig } from "next/dist/server/config-shared";
+import React from "react";
+
+export default function ForgotPassword() {
+    return(
+        <h1>Forgot Password</h1>
+    )
+}
