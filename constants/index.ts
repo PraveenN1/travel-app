f817ba9a804db03914b2397b24a2a980231a0021
PraveenN1 <praveenpu46@gmail.com>
2024-07-01 03:18:23 +0530

    Updated index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,134 +1,180 @@
 // NAVIGATION
 export const NAV_LINKS = [
-  { href: '/', key: 'home', label: 'Home' },
-  { href: '/works', key: 'how_hilink_work', label: 'How Hilink Work?' },
-  { href: '/services', key: 'services', label: 'Services' },
-  { href: '/pricing', key: 'pricing ', label: 'Pricing ' },
-  { href: '/contact-us', key: 'contact_us', label: 'Contact Us' },
+  { href: "/", key: "home", label: "Home" },
+  { href: "/works", key: "how_hilink_work", label: "How Hilink Work?" },
+  { href: "/services", key: "services", label: "Services" },
+  { href: "/pricing", key: "pricing ", label: "Pricing " },
+  { href: "/contact-us", key: "contact_us", label: "Contact Us" },
 ];
 
 // CAMP SECTION
 export const PEOPLE_URL = [
-  '/person-1.png',
-  '/person-2.png',
-  '/person-3.png',
-  '/person-4.png',
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
 ];
 
 // FEATURES SECTION
 export const FEATURES = [
   {
-    title: 'Real maps can be offline',
-    icon: '/map.svg',
-    variant: 'green',
+    title: "Real maps can be offline",
+    icon: "/map.svg",
+    variant: "green",
     description:
-      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+      "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
   },
   {
-    title: 'Set an adventure schedule',
-    icon: '/calendar.svg',
-    variant: 'green',
+    title: "Set an adventure schedule",
+    icon: "/calendar.svg",
+    variant: "green",
     description:
       "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
   },
   {
-    title: 'Technology using augment reality',
-    icon: '/tech.svg',
-    variant: 'green',
+    title: "Technology using augment reality",
+    icon: "/tech.svg",
+    variant: "green",
     description:
-      'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+      "Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection",
   },
   {
-    title: 'Many new locations every month',
-    icon: '/location.svg',
-    variant: 'orange',
+    title: "Many new locations every month",
+    icon: "/location.svg",
+    variant: "orange",
     description:
-      'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+      "Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing",
   },
 ];
 
 // FOOTER SECTION
 export const FOOTER_LINKS = [
   {
-    title: 'Learn More',
+    title: "Learn More",
     links: [
-      'About Hilink',
-      'Press Releases',
-      'Environment',
-      'Jobs',
-      'Privacy Policy',
-      'Contact Us',
+      "About Hilink",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
     ],
   },
   {
-    title: 'Our Community',
-    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    title: "Our Community",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
   },
 ];
 
 export const FOOTER_CONTACT_INFO = {
-  title: 'Contact Us',
+  title: "Contact Us",
   links: [
-    { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "hilink@akinthil.com" },
   ],
 };
 
 export const SOCIALS = {
-  title: 'Social',
+  title: "Social",
   links: [
-    '/facebook.svg',
-    '/instagram.svg',
-    '/twitter.svg',
-    '/youtube.svg',
-    '/wordpress.svg',
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
   ],
 };
 
 export const HERO = {
-  title:"Halcyon Haven Camp Area",
-  label:"Embark on a journey with WanderLust Camping Adventures, your ultimate guide to exploring the great outdoors. Whether you're a seasoned camper or new to the wilderness, we offer personalized experiences to suit every adventurer's needs."
+  title: "Halcyon Haven Camp Area",
+  label:
+    "Embark on a journey with WanderLust Camping Adventures, your ultimate guide to exploring the great outdoors. Whether you're a seasoned camper or new to the wilderness, we offer personalized experiences to suit every adventurer's needs.",
 };
 
-export const SERVICES =[
+export const SERVICES = [
   {
-    title:'Custom Camping Packages',
-    image:'/service1.png',
-    description:'Tailored camping experiences to meet individual preferences, including location, duration, and activities',
+    title: "Custom Camping Packages",
+    image: "/service1.png",
+    description:
+      "Tailored camping experiences to meet individual preferences, including location, duration, and activities",
   },
   {
-    title:'Guided Tours and Activities',
-    image:'/service2.png',
-    description:"Professional guides for hiking, fishing, kayaking, and other outdoor activities"
+    title: "Guided Tours and Activities",
+    image: "/service2.png",
+    description:
+      "Professional guides for hiking, fishing, kayaking, and other outdoor activities",
   },
   {
-    title:'Equipment Rental',
-    image:'/service3.jpg',
-    description:"High-quality camping gear rental, including tents, sleeping bags, cooking equipment, and more..."
+    title: "Equipment Rental",
+    image: "/service3.jpg",
+    description:
+      "High-quality camping gear rental, including tents, sleeping bags, cooking equipment, and more...",
   },
   {
-    title:'Transportation Services',
-    image:'/service4.png',
-    description:"Shuttle services to and from the campsite.Transportation for guided tours and activities."
+    title: "Transportation Services",
+    image: "/service4.png",
+    description:
+      "Shuttle services to and from the campsite.Transportation for guided tours and activities.",
   },
   {
-    title:'Meal Plans and Cooking Services',
-    image:'/service5.png',
-    description:"Pre-prepared meal plans with options for breakfast, lunch, and dinner"
+    title: "Meal Plans and Cooking Services",
+    image: "/service5.png",
+    description:
+      "Pre-prepared meal plans with options for breakfast, lunch, and dinner",
   },
   {
-    title:'Safety and Emergency Support',
-    image:'/service6.png',
-    description:"24/7 emergency support and assistance.On-site first aid and safety briefings."
+    title: "Safety and Emergency Support",
+    image: "/service6.png",
+    description:
+      "24/7 emergency support and assistance.On-site first aid and safety briefings.",
   },
   {
-    title:'Family and Group Packages:',
-    image:'/service7.png',
-    description:"Special packages for families and large groups, including team-building activities and family-friendly adventures."
+    title: "Family and Group Packages:",
+    image: "/service7.png",
+    description:
+      "Special packages for families and large groups, including team-building activities and family-friendly adventures.",
   },
   {
-    title:'Educational Workshops and Programs:',
-    image:'/service8.png',
-    description:"Workshops on outdoor skills, survival techniques, and environmental education.Programs for children and adults to learn about nature and wildlife."
+    title: "Educational Workshops and Programs:",
+    image: "/service8.png",
+    description:
+      "Workshops on outdoor skills, survival techniques, and environmental education.Programs for children and adults to learn about nature and wildlife.",
   },
-]
+];
+
+export const PRICING = [
+  {
+    title: "Basic",
+    description:
+      "Ideal for solo travelers or couples looking for a simple and affordable camping experience. Includes basic amenities and equipment rental",
+    price: "$99",
+    includes: [
+      "Tent and sleeping bag",
+      "Basic cooking equipment",
+      "Access to campsite facilities",
+    ],
+  },
+  {
+    title: "Standard",
+    description:
+      "Perfect for families or small groups who want a comfortable camping experience. Includes additional amenities and guided tours.",
+    price: "$199",
+    includes: [
+      "Tent, sleeping bag, and air mattress",
+      "Cooking equipment and utensils",
+      "Guided hiking tour",
+      "Transportation to campsite",
+    ],
+  },
+  {
+    title:' Premium',
+    description:'Designed for those seeking a luxurious camping experience with all-inclusive amenities and personalized services.',
+    price:'$299',
+    includes:[
+      'Luxury tent and bedding',
+      'Guided tours and activities',
+      'Transportation to and from campsite',
+      '24/7 support and assistance',
+    ]
+  }
+];
