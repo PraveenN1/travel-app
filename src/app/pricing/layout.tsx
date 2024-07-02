1a5caa4019bfeeeb9f1e5624666f77aec7e1b7ad
PraveenN1 <praveenpu46@gmail.com>
2024-07-02 14:58:20 +0530

    Update pricing.tsx

--- src/app/pricing/layout.tsx
+++ src/app/pricing/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "TravelApp | Pricing",
+  title: "Pricing | Hilink",
   description: "pricing plans",
 };
 
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { PRICING } from "../../../constants";
 import Image from "next/image";
 import Button from "@/components/Button";
+import Link from "next/link";
 
 export default function Pricing() {
     return (
-        <div className="flex flex-col items-center justify-center bg-gray-100 p-5 ">
-            <h1 className="text-6xl text-center text-balance font-bold mb-4">Pricing Plans</h1> 
-            <h2 className="text-2xl text-center font-semibold mb-2">Choose a plan that's right for you</h2>
-            <p className="text-lg m-3 text-balance text-center">We offer a variety of pricing plans to suit different needs and preferences. Whether you're looking for a basic camping experience or an all-inclusive adventure, we have a plan for you.</p>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ">
+        <div className="flex flex-col items-center justify-center bg-gray-100 p-5">
+            <h1 className="text-5xl md:text-6xl lg:text-7xl text-center text-balance font-bold mb-4">Pricing Plans</h1>
+            <h2 className="text-xl md:text-2xl  text-center font-semibold mb-2">Choose a plan that's right for you</h2>
+            <p className="text-sm md:text-md text-gray-50 w-auto italic m-3 text-balance text-center">We offer a variety of pricing plans to suit different needs and preferences. Whether you're looking for a basic camping experience or an all-inclusive adventure, we have a plan for you.</p>
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-full px-4">
                 {PRICING.map((plan, index) => (
                     <PriceCard
                         key={index}
@@ -32,11 +33,12 @@ type PriceCardProps = {
 }
 
 const PriceCard = ({ title, description, price, includes }: PriceCardProps) => {
+    const isStandard = title === "Standard";
     return (
-        <div className="relative px-8 h-82 w-76 flex flex-col justify-between items-center gap-2 border-2 border-black font-serif rounded-lg shadow-md">
-            <div className="flex flex-col justify-start items-center gap-2 mt-4">
-                <h3 className="text-3xl text-center">{title}</h3>
-                <p className="text-5xl font-bold text-center">{price}</p>
+        <div className={`relative my-2 px-8 pb-6 h-auto w-full flex flex-col justify-between items-center gap-2 border-2 font-serif rounded-lg shadow-md ${isStandard ? 'shadow-2xl transform scale-105 border-green-500' : 'shadow-md border-gray-300'} hover:shadow-2xl transition-all duration-300`}>
+            <div className="flex flex-col justify-start items-center gap-2">
+                <h3 className={`text-3xl p-2 text-center rounded-t-[0.4rem] text-white w-full absolute ${title === "Basic" ? 'bg-green-600' : title === "Standard" ? 'bg-green-500' : 'bg-green-600'}`}>{title}</h3>
+                <p className="text-5xl font-bold text-center pt-12">{price}</p>
                 <p className="italic text-center mb-4">{description}</p>
                 <ul className="text-left list-none p-0 mb-4 w-full">
                     {includes.map((item, index) => (
@@ -46,20 +48,22 @@ const PriceCard = ({ title, description, price, includes }: PriceCardProps) => {
                                 alt="tick"
                                 width={20}
                                 height={20}
-                                className="mr-2"
+                                className="mr-2 rounded-full"
                             />
                             {item}
                         </li>
                     ))}
                 </ul>
             </div>
-            <span className="bottom-0 pb-2">
-                <Button
-                    type="button"
-                    title="Buy Now"
-                    variant="btn_green"
-                />
-            </span>
+            <div className="bottom-0 pb-2 shadow-sm">
+                <Link href="#">
+                    <Button
+                        type="button"
+                        title="Buy Now"
+                        variant="btn_dark_green"
+                    />
+                </Link>
+            </div>
         </div>
     );
 }
