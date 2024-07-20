4f516374167c85d68eec6b468af2896b01342012
PraveenN1 <praveenpu46@gmail.com>
2024-07-21 01:52:02 +0530

    updated login redirect page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,17 +5,8 @@ import Image from "next/image";
 
 export default function SignIn() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-sm p-6 bg-white rounded shadow-md">
-        <div className="flex justify-center">
-          <Image
-            src="/hilink-logo.svg"
-            alt="Logo"
-            width={80}
-            height={80}
-            className="mb-4"
-          />
-        </div>
+    <div className="flex items-center justify-center min-h-screen  bg-gray-100">
+      <div className="w-full max-w-sm p-10 bg-white rounded shadow-md">
         <h2 className="text-2xl font-bold text-center">Welcome to TravelApp</h2>
         <p className="mt-2 text-center text-gray-600">
           Please sign in to continue
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -50,7 +58,29 @@ module.exports = {
       fontFamily:{
         playwrite:["Playwrite DE Grund", 'cursive'],
       },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
